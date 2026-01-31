0ac089dcad4095c3fcda54c3a71b1d4aa41d9d60
Fran <fmarchantemera@safareyes.es>
2026-01-31 18:52:27 +0100

    Despliegue android

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,10 @@
-export const environment = {
+/*export const environment = {
   production: false,
   apiUrl: 'http://localhost:8080'
+};
+ */
+
+export const environment = {
+  production: false,
+  apiUrl: 'http://10.0.2.2:8080'
 };
