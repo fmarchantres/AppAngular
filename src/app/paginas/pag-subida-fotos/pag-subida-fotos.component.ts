bd35963ce03edff25ead407ee352b9b1319ab367
Fran <fmarchantemera@safareyes.es>
2026-02-08 13:22:11 +0100

    Solicita permisos

--- src/app/paginas/pag-subida-fotos/pag-subida-fotos.component.ts
+++ src/app/paginas/pag-subida-fotos/pag-subida-fotos.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { PlantillaComponent } from "../plantilla/plantilla.component";
 import { FormsModule } from "@angular/forms";
 import { IonSelect, IonSelectOption, IonCard, IonCardContent, IonLabel, IonInput, IonButton } from "@ionic/angular/standalone";
+import { Camera, CameraResultType, CameraSource } from '@capacitor/camera';
+
 
 @Component({
   selector: 'app-pag-subida-fotos',
@@ -31,5 +33,28 @@ export class PagSubidaFotosComponent {
     console.log("Descripción:", this.descripcion);
     console.log("Fotos seleccionadas:", this.fotos);
   }
+
+  async abrirCamara() {
+    const image = await Camera.getPhoto({
+      quality: 80,
+      allowEditing: false,
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera
+    });
+
+    if (image.webPath) {
+      const response = await fetch(image.webPath);
+      const blob = await response.blob();
+
+      const file = new File(
+        [blob],
+        `foto_${Date.now()}.jpg`,
+        { type: blob.type }
+      );
+
+      this.fotos.push(file);
+    }
+  }
+
 
 }
